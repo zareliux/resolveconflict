0253f2766ed0474a8dacc5cb81caf3d3f787b2f7
zareliux <zarela.rojas@Zarelas-MacBook-Pro.local>
2024-12-17 01:04:28 -0500

    dev1commit

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,11 @@ public class HelloJava {
 	
 	public static void main(String[] args) {
 
+		System.out.println("Inicio Dev1");
 		System.out.println("Hello Java");
+		System.out.println("Hello Mundo");
+		System.out.println("Fin Dev1");
+
 		  
 		  
 	
